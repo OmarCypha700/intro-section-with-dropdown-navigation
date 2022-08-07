4cec467d8b2ca836b864bbac318206bd64b511fb
OmarCypha700 <98944012+OmarCypha700@users.noreply.github.com>
2022-08-06 22:54:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-//  function dropFeatures () {
-//     document.getElementsByClassName('features').classList.toggle('features');
-//  }
-
-//  window.onclick = function (e) {
-//     if(!e.target.matches('.dropbtn')) {
-//         let drop = document.getElementById('drop');
-//         if(drop.classList.contains('show')) {
-//             drop.classList.remove('show')
-//         }
-//     }
-//  }
-
-// window.onload =  () => {
-//     const hamburger_menu = document.querySelector('#menu').src;
-//     const mobile_menu = document.querySelector('.mobile-nav');
-
-//     function toggleImg () {
-//         if(hamburger_menu.indexOf("./images/icon-menu.svg") != -1){
-//             document.querySelector("#menu").src = "./images/icon-close-menu.svg"
-//         }
-//         else{
-//             document.querySelector("#menu").src="./images/icon-menu.svg"
-//         }
-//         // hamburger_menu.src="./images/icon-close-menu.svg";
-//         mobile_menu.classList.toggle('is-active');
-//     };
-
-
-// }
-
 
 window.onload =  () => {
     const hamb_menu = document.querySelector('.hamburger');
